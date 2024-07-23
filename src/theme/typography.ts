847abea6c5bbc0432d79f3f8d2d0b93a6ab72e24
a.Nox <aurulenox@gmail.com>
2024-07-23 20:39:56 +0900

    rollback

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,5 +1,4 @@
-// import { Barlow, Public_Sans } from 'next/font/google';
-import { Public_Sans } from 'next/font/google';
+import { Barlow, Public_Sans } from 'next/font/google';
 
 // ----------------------------------------------------------------------
 
@@ -39,12 +38,12 @@ export const primaryFont = Public_Sans({
   fallback: ['Helvetica', 'Arial', 'sans-serif'],
 });
 
-// export const secondaryFont = Barlow({
-//   weight: ['400', '500', '600', '700', '800', '900'],
-//   subsets: ['latin'],
-//   display: 'swap',
-//   fallback: ['Helvetica', 'Arial', 'sans-serif'],
-// });
+export const secondaryFont = Barlow({
+  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
 
 // ----------------------------------------------------------------------
 
