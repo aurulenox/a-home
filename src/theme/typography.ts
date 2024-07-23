6041a657a558fa028b1f1620043dd4bcd1036de9
a.Nox <aurulenox@gmail.com>
2024-07-23 20:36:18 +0900

    font error

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,4 +1,5 @@
-import { Barlow, Public_Sans } from 'next/font/google';
+// import { Barlow, Public_Sans } from 'next/font/google';
+import { Public_Sans } from 'next/font/google';
 
 // ----------------------------------------------------------------------
 
@@ -38,12 +39,12 @@ export const primaryFont = Public_Sans({
   fallback: ['Helvetica', 'Arial', 'sans-serif'],
 });
 
-export const secondaryFont = Barlow({
-  weight: ['400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
-});
+// export const secondaryFont = Barlow({
+//   weight: ['400', '500', '600', '700', '800', '900'],
+//   subsets: ['latin'],
+//   display: 'swap',
+//   fallback: ['Helvetica', 'Arial', 'sans-serif'],
+// });
 
 // ----------------------------------------------------------------------
 
